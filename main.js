f12db31bec1aaca7adbf736ea2216c3befc6a1e4
Cheng-Kai-Ming <k_sky369@yahoo.com.tw>
2022-03-19 22:58:31 +0800

    Delete the end of character

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 document.addEventListener("DOMContentLoaded", () =>{
   const keyboard = document.querySelector('.keyboard')
   const screen = document.querySelector('#screen')
+  const delete_word = document.querySelector('#delete')
   let content = ''
 
   keyboard.addEventListener("click", (e) =>{
-    // console.log(e.target.innerText)
-    // console.log(screen.innerHTML)
     content += e.target.innerText
     screen.innerHTML = content
     })
+
+  delete_word.addEventListener("click", () =>{
+    let delete_character = content.length - 1
+    content = content.slice(0,delete_character)
+    screen.innerHTML = content
+  })
 
   }
 )
